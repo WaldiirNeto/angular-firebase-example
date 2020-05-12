70965a8ea7793a49d88bedf095c3bd64ab34c43b
waldir <nwalldir@gmail.com>
2020-05-12 10:44:57 -0400

    adicionado tela de login e modules compartilhados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderModule } from './shared/header/header.module';
+import { FooterModule } from './shared/footer/footer.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule,
+    FooterModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
